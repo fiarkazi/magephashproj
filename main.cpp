9d149a55e5cc2a2073a8ccb1cebd6ccec6137c95
fiarkazi <36688890+fiarkazi@users.noreply.github.com>
2018-03-01 12:34:23 +0300

    console kek

--- main.cpp
+++ main.cpp
@@ -670,14 +670,155 @@ double ph_hammingdistance2(uint8_t *hashA, int lenA, uint8_t *hashB, int lenB){
 
 }
 
-/*--------------------------------------------------------------------
-int main(int argc, char **argv)
-{
+/*
+	thx stackoverflow :^)
+*/
+using namespace std;
 
-	 TO-DO:
-	-обработка файлов(readfilenames)
-	-вывод хэшей изображений в отдельный файл для каждой(xml format)
-	-интерфейс(qt?)
-    
+#define TRUE 1
+#define FALSE 0
+
+//структура для хэша и айди иоз
+struct ph_imagepoint{
+    ulong64 hash;
+    char *id;
+};
+
+//aux function to create imagepoint data struct
+struct ph_imagepoint* ph_malloc_imagepoint(){
+
+    return (struct ph_imagepoint*)malloc(sizeof(struct ph_imagepoint));
+
 }
-*---------------------------------------------------------------------/
+
+//Дополнительная функция для сортировки списка хэшей
+bool cmp_lt_imp(struct ph_imagepoint dpa, struct ph_imagepoint dpb){
+    int result = strcmp(dpa.id, dpb.id);
+    if (result < 0)
+	return TRUE;
+    return FALSE;
+}
+\
+int main(int argc, char **argv){
+
+
+    if (argc < 3){
+	printf("no input args\n");
+	printf("expected: \"test_imagephash [dir name] [dir_name]\"\n");
+	exit(1);
+    }
+    const char *dir_name = argv[1];
+    const char *dir_name2 = argv[2];
+    struct dirent *dir_entry;
+    vector<ph_imagepoint> hashlist1; //for hashes in first directory
+    vector<ph_imagepoint> hashlist2; //for hashes in second directory
+    ph_imagepoint *dp = NULL;
+
+    //first directory
+    DIR *dir = opendir(dir_name);
+    if (!dir){
+	printf("unable to open directory\n");
+	exit(1);
+    }
+    errno = 0;
+    int i = 0;
+    ulong64 tmphash;
+    char path[100];
+    path[0] = '\0';
+    while ((dir_entry = readdir(dir)) != 0){
+	if (strcmp(dir_entry->d_name,".") && strcmp(dir_entry->d_name,"..")){
+	    strcat(path, dir_name);
+	    strcat(path, "/");
+	    strcat(path, dir_entry->d_name);
+	    if (ph_dct_imagehash(path, tmphash) < 0)  //calculate the hash
+		continue;
+            dp = ph_malloc_imagepoint();              //store in structure with file name
+	    dp->id = dir_entry->d_name;
+	    dp->hash = tmphash;
+	    hashlist1.push_back(*dp);
+	    i++;
+	}
+	errno = 0;
+        path[0]='\0';
+    }
+
+    if (errno){
+	printf("error reading directory\n");
+	exit(1);
+    }
+
+    sort(hashlist1.begin(),hashlist1.end(),cmp_lt_imp);
+
+    //second directory
+    dir_entry = NULL;
+    DIR *dir2 = opendir(dir_name2);
+    if (!dir){
+	printf("unable to open directory\n");
+	exit(1);
+    }
+    errno = 0;
+    path[0] = '\0';
+    i=0;
+    while ((dir_entry = readdir(dir2)) != 0){
+	if (strcmp(dir_entry->d_name,".") && strcmp(dir_entry->d_name,"..")){
+	    strcat(path,dir_name2);
+	    strcat(path,"/");
+	    strcat(path,dir_entry->d_name);
+	    if (ph_dct_imagehash(path,tmphash) < 0)    //calculate the hash
+		continue;
+    	    dp = ph_malloc_imagepoint();               //store in structure with filename
+	    dp->id = dir_entry->d_name;
+	    dp->hash = tmphash;
+	    hashlist2.push_back(*dp);
+	    i++;
+	}
+	errno = 0;
+	path[0] = '\0';
+    }
+
+    if (errno){
+	printf("error reading directory\n");
+	exit(1);
+    }
+
+    sort(hashlist2.begin(),hashlist2.end(),cmp_lt_imp);
+
+    int nbfiles1 = hashlist1.size();
+    int nbfiles2 = hashlist2.size();
+    int nbfiles = nbfiles1;
+    if (nbfiles1 != nbfiles2){
+	nbfiles = (nbfiles2 > nbfiles1) ? nbfiles2:nbfiles1;
+    }
+    
+    int distance = -1;
+    printf("**************************\n");
+    printf("similar images distance comparisons\n");
+    printf("**************************\n");
+    for (i=0;i<nbfiles;i++){
+	printf(" %d %s %s ",i,hashlist1[i].id, hashlist2[i].id);
+
+	//calculate distance
+	distance = ph_hamming_distance(hashlist1[i].hash,hashlist2[i].hash);
+
+	printf(" dist = %d\n",distance);
+    }
+
+
+    printf("**************************\n");
+    printf("different distance comparisons\n");
+    printf("**************************\n");
+    for (i=0;i<nbfiles1;i++){
+	for (int j=i+1;j<nbfiles1;j++){
+	    printf(" %s %s ", hashlist1[i].id, hashlist1[j].id);
+
+	    //calculate distance
+	    distance = ph_hamming_distance(hashlist1[i].hash,hashlist1[j].hash);
+
+	    printf(" dist = %d\n",distance);
+	}
+    }
+    printf("**************************\n");
+
+
+    return 0;
+}
